a8d665275457dd757a31133ec0cd3bd792930c11
James Curran <dzejms@gmail.com>
2018-12-12 05:16:49 -0500

    Added domain model

---
+++ 2-dotnet-micro-orms-m2-dapper-exercise-files/demos/before/MicroORMDemo/DomainModel/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DomainModel
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public string Title { get; set; }
+    }
+}
