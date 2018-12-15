a68f0d359220e1d3e41ca436cdc84ebe30f67a7a
James Curran <dzejms@gmail.com>
2018-12-15 16:18:47 -0500

    Switched to sprocs

--- 2-dotnet-micro-orms-m2-dapper-exercise-files/demos/before/MicroORMDemo/DataLayer/Persistence/DapperContactsRepository.cs
+++ 2-dotnet-micro-orms-m2-dapper-exercise-files/demos/before/MicroORMDemo/DataLayer/Persistence/DapperContactsRepository.cs
@@ -45,13 +45,14 @@ namespace MicroOrmDemo.DataLayer.Persistence
 
         public Contact GetFullContact(int id)
         {
-            string selectStatement =
-            @"
-                Select * from Contacts Where Id = @Id;
-                Select * from Addresses Where ContactId = @Id;
-            ";
+            //string selectStatement =
+            //@"
+            //    Select * from Contacts Where Id = @Id;
+            //    Select * from Addresses Where ContactId = @Id;
+            //";
 
-            using (var multipleResults = this.dbConnection.QueryMultiple(selectStatement, new { Id = id }))
+            //using (var multipleResults = this.dbConnection.QueryMultiple(selectStatement, new { Id = id }))
+            using (var multipleResults = this.dbConnection.QueryMultiple("GetContact", new { Id = id }, commandType: CommandType.StoredProcedure))
             {
                 Contact contact = multipleResults.Read<Contact>().SingleOrDefault();
                 List<Address> addresses = multipleResults.Read<Address>().ToList();
@@ -74,34 +75,63 @@ namespace MicroOrmDemo.DataLayer.Persistence
         {
             using (TransactionScope scope = new TransactionScope())
             {
-                if (contact.IsNew)
-                {
-                    Add(contact);
-                }
-                else
-                {
-                    Update(contact);
-                }
+                //if (contact.IsNew)
+                //{
+                //    Add(contact);
+                //}
+                //else
+                //{
+                //    Update(contact);
+                //}
+
+                DynamicParameters saveContactParams = new DynamicParameters();
+                saveContactParams.Add("@Id", value: contact.Id, dbType: DbType.Int32, direction: ParameterDirection.InputOutput);
+                saveContactParams.Add("@FirstName", contact.FirstName);
+                saveContactParams.Add("@LastName", contact.LastName);
+                saveContactParams.Add("@Company", contact.Company);
+                saveContactParams.Add("@Title", contact.Title);
+                saveContactParams.Add("@Email", contact.Email);
+                this.dbConnection.Execute("SaveContact", saveContactParams, commandType: CommandType.StoredProcedure);
+                contact.Id = saveContactParams.Get<int>("@Id");
 
                 foreach (Address address in contact.Addresses.Where(ad => !ad.IsDeleted))
                 {
                     address.ContactId = contact.Id;
 
-                    if (address.IsNew)
+                    //if (address.IsNew)
+                    //{
+                    //    Add(address);
+                    //}
+                    //else
+                    //{
+                    //    Update(address);
+                    //}
+
+                    DynamicParameters saveAddressParams = new DynamicParameters(new
                     {
-                        Add(address);
-                    }
-                    else
-                    {
-                        Update(address);
-                    }
+                        address.ContactId,
+                        address.AddressType,
+                        address.StreetAddress,
+                        address.City,
+                        address.StateId,
+                        address.PostalCode
+                    });
+                    saveAddressParams.Add("@Id", value: address.Id, dbType: DbType.Int32, direction: ParameterDirection.InputOutput);
+
+                    this.dbConnection.Execute("SaveAddress", saveAddressParams, commandType: CommandType.StoredProcedure);
+                    address.Id = saveAddressParams.Get<int>("@Id");
                 }
 
-                if (contact.Addresses.Any(ad => ad.IsDeleted))
+                //if (contact.Addresses.Any(ad => ad.IsDeleted))
+                //{
+                //    string deleteStatement = "Delete from Addresses where Id in @Ids";
+                //    IEnumerable<int> ids = contact.Addresses.Where(ad => ad.IsDeleted).Select(ad => ad.Id);
+                //    dbConnection.Execute(deleteStatement, new { Ids = ids });
+                //}
+
+                foreach (var address in contact.Addresses.Where(addr => addr.IsDeleted))
                 {
-                    string deleteStatement = "Delete from Addresses where Id in @Ids";
-                    IEnumerable<int> ids = contact.Addresses.Where(ad => ad.IsDeleted).Select(ad => ad.Id);
-                    dbConnection.Execute(deleteStatement, new { Ids = ids });
+                    this.dbConnection.Execute("DeleteAddress", new { Id = address.Id }, commandType: CommandType.StoredProcedure);
                 }
 
                 scope.Complete();
